f021b2b8424615f85515be0bf374b569aad3cfc8
mjachi <mmmeeeker@gmail.com>
2022-08-04 16:47:24 -0700

    Tons of plugins

--- init.lua
+++ init.lua
@@ -1,5 +1,43 @@
 -- Author: Matthew Meeker
 
-require("core/init")
-require("pack/init")
-require("utils/init")
+local core_modules = {
+
+  -- Core Settings
+  "core/plugins",
+  "core/keymaps",
+  "core/options",
+
+  -- visual
+  "configs/colorscheme",
+  "configs/colorizer",
+
+  -- Plugin Configurations
+  "configs/impatient",
+  "configs/alpha-nvim",
+  "configs/nvim-tree",
+  "configs/treesitter",
+
+  "configs/LSP_configs/completion",
+  "configs/LSP_configs/aerial",
+  "configs/LSP_configs/trouble",
+
+  -- functionalities
+  "configs/gitsigns",
+  "configs/commentary",
+  "configs/smooth-scrl",
+  "configs/toggleterm",
+  "configs/lualine",
+  "configs/incline",
+  "configs/telescope",
+
+  ---- Other Utilities
+  "utils/vstuff",
+
+}
+
+for _, module in ipairs(core_modules) do
+    local ok, err = pcall(require, module)
+    if not ok then
+      return err
+    end
+end
---
+++ lua/core/plugins.lua
@@ -0,0 +1,268 @@
+-- Author: Matthew Meeker
+
+
+local fn = vim.fn
+
+-- If packer is not found in the path described below, installs and asks the user
+-- to restart nvim
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- pcall on Packer and initialization
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+
+-- These define bits s.t. commands upon which to load plugins
+local treesitter_cmds = {
+  "TSInstall",
+  "TSBufEnable",
+  "TSBufDisable",
+  "TSEnable",
+  "TSDisable",
+  "TSModuleInfo",
+}
+
+local mason_cmds = {
+  "Mason",
+  "MasonInstall",
+  "MasonInstallAll",
+  "MasonUninstall",
+  "MasonUninstallAll",
+  "MasonLog",
+}
+
+
+-- Finally, call Packer with the following packages
+return packer.startup(function(use)
+
+  -- Core
+  use "wbthomason/packer.nvim"
+  use "wakatime/vim-wakatime"
+  use "lewis6991/impatient.nvim"
+  use {
+    "nvim-lua/plenary.nvim",
+    module = "plenary"
+  }
+  use {
+    'eraserhd/parinfer-rust',
+    opt = true,
+    run = 'cargo build --release'
+  }
+
+  -- Aesthetics
+  use "norcalli/nvim-colorizer.lua"
+  use {
+    "kyazdani42/nvim-web-devicons",
+    module = "nvim-web-devicons"
+  }
+  --- Colorschemes
+  use "shaunsingh/nord.nvim"
+  use "sainnhe/everforest"
+  use "morhetz/gruvbox"
+  use "folke/tokyonight.nvim"
+  use "catppuccin/nvim"
+  use "EdenEast/nightfox.nvim"  
+  use {
+    'shaunsingh/oxocarbon.nvim',
+    run = './install.sh'
+  }
+
+  use "lukas-reineke/indent-blankline.nvim"
+
+  -- Mappings
+  use {
+    "anuvyklack/hydra.nvim",
+    keys = "<space>",
+    -- config = load file
+  }
+  use { -- Autopairs e.g. {}, [], (), "", ''
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require "configs/autopairs"
+    end
+  }
+  use { -- Faster navigation around a buffer
+    "ggandor/leap.nvim",
+    setup = function()
+      require('leap').set_default_keymaps()
+    end,
+    config = function()
+
+    end
+  }
+  use { -- Faster un/commenting for lines
+    "terrortylor/nvim-comment"
+  }
+  use "karb94/neoscroll.nvim"
+
+
+  -- File Navigation
+  -- nvim tree
+  use {
+    "kyazdani42/nvim-tree.lua",
+    cmd = "NvimTreeToggle",
+    config = function()
+      -- require the nvim tree setup file
+    end
+  }
+  -- telescope: telescope-project, telescope-ui-select, telescope-fzfnative
+  
+  -- Treesitter
+  use { -- Treesitter and related subpackages
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    cmd = treesitter_cmds,
+    module = "nvim-treesitter",
+    config = function()
+      -- 
+    end,
+    setup = function()
+
+    end,
+    requires = {
+      {"nvim-treesitter/playground", cmd = "TSPlayground"},
+      {"p00f/nvim-ts-rainbow", after = "nvim-treesitter"},
+      {"nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter"}
+    }
+  }
+  
+  -- LSP
+  use { -- Mason package manager
+    "williamboman/mason.nvim",
+    cmd = mason_cmds,
+    config = function()
+      require "configs/mason"
+    end
+  }
+  use {
+    "j-hui/fidget.nvim",
+    after = "nvim-lspconfig",
+    -- config = call setup
+  }
+  use {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    after = "nvim-lspconfig",
+    -- config = call setup for lsp_lines
+  }
+  use {
+    "neovim/nvim-lspconfig",
+    opt = true,
+    setup = function()
+      -- TODO -- lazy loading things
+    end,
+    config = function()
+      require("configs/lsp")
+    end
+
+  }
+  
+
+  -- Git
+  use { -- Neogit
+    "TimUntersberger/neogit",
+    config = function()
+      -- TODO -- Call setup
+    end,
+  }
+  use { -- Gitsigns
+    "lewis6991/gitsigns.nvim",
+    ft = "gitcommit",
+    -- config = call setup
+    -- setup = lazy loading things
+  }
+  
+  -- Completion
+  use { -- nvim-cmp so nice!
+    "hrsh7th/nvim-cmp",
+    -- config = load file cmp
+    wants = "LuaSnip",
+    even = "InsertEnter",
+    requires = {
+      {"hrsh7th/cmp-path", after = "nvim-cmp"},
+      {"hrsh7th/cmp-buffer", after = "nvim-cmp"},
+      {"hrsh7th/cmp-nvim-lsp", after = "nvim-cmp"},
+      {"saadparwaiz1/cmp_luasnip", after = "nvim-cmp"},
+      {"lukas-reineke/cmp-under-comparator", module = "cmp-under-comparator"},
+      {"L3MON4D3/LuaSnip", 
+        event = "InsertEnter", 
+        wants = "friendly-snippets", 
+        -- config = load-file luasnip
+        requires = {
+          {"rafamadriz/friendly-snippets"}
+        }
+      },
+    }
+  }
+
+  -- More aesthetic items
+  use { -- Zen mode for writing
+    "Pocco81/true-zen.nvim",
+    cmd = "TZAtaraxis",
+    -- config = load file
+  }
+  use { -- Match parentheses
+    "monkoose/matchparen.nvim",
+    opt = true,
+    -- config = load file
+    -- TODO for setup -> lazyload things
+  }
+  use { -- Better notification system/ pop-up's
+    "rcarriga/nvim-notify",
+    module = "notify",
+    opt = true,
+    setup = function()
+      vim.notify = function(msg, level, opts)
+        packer.loader("nvim-notify")
+        vim.notify = require("notify")
+        vim.notify(msg, level, opts)
+      end
+      return
+    end
+  }
+  use { "b0o/incline.nvim" }
+
+
+  -- Orgmode
+  use { -- Neorg
+    "nvim-neorg/neorg",
+    config = function()
+      require("configs/neorg")
+    end,
+    ft = "neorg",
+    after = "nvim-treesitter"
+  }
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
