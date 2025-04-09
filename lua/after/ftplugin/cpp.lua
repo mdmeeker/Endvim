c77fc52ef65355f0ce499fcbdb0c5a97723e9f02
Matthew Meeker <mmmeeeker@gmail.com>
2025-04-09 17:48:36 -0400

    Restarting to base off of astronvim

--- lua/after/ftplugin/cpp.lua
+++
@@ -1,4 +0,0 @@
--- C++ specific settings
-vim.opt_local.expandtab = true     -- Use spaces
-vim.opt_local.tabstop = 2          -- 2 space indentation
-vim.opt_local.shiftwidth = 2 
--- lua/core/init.lua
+++
@@ -1,6 +0,0 @@
--- Add this line wherever you initialize your plugins
-require("plugins.indent-blankline").setup()
-
--- Near the top of the file, after loading plugins but before configuring them
-local snippets = require("plugins.snippets")
-snippets.setup() 
